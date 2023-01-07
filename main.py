d2bf6add33aa3257aa289735baf90ba4181a71ae
KBogielMiko <kacpibm@gmail.com>
2023-01-07 18:59:26 +0100

    init commit

--- main.py
+++ main.py
@@ -1,37 +1,32 @@
-# import neccessary libraries
-
+import getpass
+import kucoin
 import sched
 import time
-import kucoin
-import getpass
 from datetime import datetime, timedelta
 from kucoin.client import Trade
 
 # parameters for Buy Limit transaction
 
-ticker = ''         # string, for example: 'BTC-USDT'
-price = '2.1610'    # string, for example: '16056.78'
-amount = '0.5'      # string, for example: '0.5'
+ticker = ''         # string - pair we are interested in, for example: 'BTC-USDT'
+price = ''          # string - price we are interested to create Buy Limit order,for example: '16056.78'
+amount = ''         # string - amount we are interested to buy, for example: '0.5' (half of the coin)
 
-api_key = getpass.getpass()              # api key of the user
-api_secret = getpass.getpass()           # secret key of the user
+api_key = ''                             # api key of the user
+api_secret = ''                          # secret key of the user
+print("Please enter your password")
 api_passphrase = getpass.getpass()       # passphrase of the user
+print(api_passphrase)
+quit()
 
 # enter to API
 client = Trade(key=api_key, secret=api_secret, passphrase=api_passphrase, is_sandbox=False, url='https://api.kucoin.com')
 
-year = 2023
-month = 1
-day = 5
-hour = 19
-minute = 8
-second = 0
-
 #schedule of transaction
+date = ''           # string - date we are interested to create Buy Limit order, for example: '2023-01-06 20:03:00.043' (date has to be given in exact format)
 
 s = sched.scheduler(time.time, time.sleep)
 now = datetime.now()
-desired_execution_time = time.mktime((year, month, day, hour, minute, second,0,0,0))
+desired_execution_time = time.mktime((int(data[:4]), int(data[5:7]), int(data[8:10]), int(data[11:13]), int(data[14:16]), int(data[17:19]),int(data[20]),int(data[21]),int(data[22])))
 s.enterabs(desired_execution_time, 1, client.create_limit_order(ticker, 'buy', amount, price), (s,))
 
 s.run()
---
+++ main2.py
@@ -0,0 +1,34 @@
+import getpass
+import kucoin
+import sched
+import time
+from datetime import datetime, timedelta
+from kucoin.client import Trade
+
+# parameters for Buy Limit transaction
+
+ticker = 'PIAS-USDT'         # string - pair we are interested in, for example: 'BTC-USDT'
+price = '0.0679'          # string - price we are interested to create Buy Limit order,for example: '16056.78'
+amount = '20'         # string - amount we are interested to buy, for example: '0.5' (half of the coin)
+
+api_key = '63a8ab5fce40be0001afdb14'                             # api key of the user
+api_secret = '4a1cea16-6771-4456-94ae-6479334406fb'                          # secret key of the user
+print("Please enter your password")
+api_passphrase = getpass.getpass()       # passphrase of the user
+print(api_passphrase)
+# enter to API
+client = Trade(key=api_key, secret=api_secret, passphrase=api_passphrase, is_sandbox=False, url='https://api.kucoin.com')
+
+#schedule of transaction
+date = '2023-01-07 15:39:00.000'           # string - date we are interested to create Buy Limit order, for example: '2023-01-06 20:03:00.043' (date has to be given in exact format)
+
+s = sched.scheduler(time.time, time.sleep)
+
+def run_command():
+    order = client.create_limit_order(ticker, 'buy', amount, price)
+    return order
+
+run_time = time.mktime(time.strptime(date, '%Y-%m-%d %H:%M:%S.%f'))
+s.enterabs(run_time, 1, run_command(), (s,))
+
+s.run()
